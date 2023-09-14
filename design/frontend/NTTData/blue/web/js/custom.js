f0db8186652af546dc525e47ba96b3675d6437a0
tperezponisio-nttdata <tomasalejandro.perezponisio@emeal.nttdata.com>
2023-09-14 13:42:48 -0300

    Ejercicio 5: Traducciones / me falta el bonus que no se que onda

---
+++ design/frontend/NTTData/blue/web/js/custom.js
@@ -0,0 +1,8 @@
+define([
+    'jquery',
+    'mage/translate'
+], function ($, $t) {
+    // Your custom JavaScript code here
+    var translatedString = $t('Hello, this is a JS translated string.');
+});
+
