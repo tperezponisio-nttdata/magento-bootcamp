6f77787b84dc57e826515333ef145eb2a1d181d7
tperezponisio-nttdata <tomasalejandro.perezponisio@emeal.nttdata.com>
2023-09-21 17:03:47 -0300

    Ejercicio 7.3: UI Components - EL Muletiselect anda pero me tira error en consola, luchando con eso

--- design/frontend/NTTData/blue/requirejs-config.js
+++ design/frontend/NTTData/blue/requirejs-config.js
@@ -1,8 +1,16 @@
 var config = {
-  map: {
-      '*': {
-          'diego-carousel': 'js/diego-carousel',
-          'sticky': 'js/sticky'
-      }
-  }
+    // shim: {
+    //     'mage/multiselect': ['jQuery']
+    // },
+    map: {
+        '*': {
+            'diego-carousel': 'js/diego-carousel',
+            'sticky': 'js/sticky'
+        }
+    },
+    shim: {
+        'mage/multiselect': {
+            deps: ['jquery']
+        }
+    }
 };
