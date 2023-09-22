ab50ff291d4e2df160d4ac27537b24e7aac6a7e3
tperezponisio-nttdata <tomasalejandro.perezponisio@emeal.nttdata.com>
2023-09-22 13:54:34 -0300

    Ejercicio 9

--- etc/env.php
+++ etc/env.php
@@ -75,7 +75,7 @@ return [
         ]
     ],
     'x-frame-options' => 'SAMEORIGIN',
-    'MAGE_MODE' => 'developer',
+    'MAGE_MODE' => 'production',
     'session' => [
         'save' => 'redis',
         'redis' => [
